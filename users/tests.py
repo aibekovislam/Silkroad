522e8d9613f35a5823edc12519616ccb6e6746e9
islam <aibekovislam@gmail.com>
2021-07-19 13:53:43 +0300

    modified tests

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 
 
@@ -12,5 +17,38 @@ class UserLoadTestCase(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
 
+
+class UserCreateAPITest(APITestCase):
+    def setUp(self):
+        self.url = reverse('user-list')
+    def UserCreateTest(self):
+        data = {
+            'username': 'islamgg',
+            'email': 'islam11@gmail.com',
+            'password_1': "test123",
+            'password_2': "test123",
+
+        }
+
+        response = self.client.post(
+            path=self.url,
+            data=data
+        )
+
+        # status
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+        expected_data = {
+            "username": "islamgg",
+            "email": "islam11@gmail.com"
+        }
+
+        self.assertEqual(response.data, expected_data)
+
+        # правильно ли записалось
+        user = User.objects.get(username="islamgg")
+        self.assertEqual(user.username, data["username"])
+        self.assertEqual(user.email, data["email"])
 
 # Create your tests here.
