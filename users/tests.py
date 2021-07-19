05c80652734ab14d866872fab02bde273fd90ecb
islam <aibekovislam@gmail.com>
2021-07-19 13:41:27 +0300

    added test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+
+
+class UserLoadTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('user-list')
+
+    def test_homepage_loads_success(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
 
 # Create your tests here.
